ade05725b2fadb93af3cc9150e310ee1f1b2ddec
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2025-09-06 17:06:19 +0800

    add files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>RMPlaysMCYT Studios - Home</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="icon" href="../assets/assets/RM1.png" type="image/x-icon">
 </head>
 <body>
-    TEST PAGE
+    <!-- TEST PAGE
     <br>
     <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
         <a href="{{ url('/about') }}">About</a>
@@ -19,6 +20,14 @@
     <br>
     <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
         <a href="{{ url('/support') }}">Support</a>
-    </button>
+    </button> -->
+    <main>
+        <header>
+            <div class="p-4 bg-white border-b border-gray-200">
+                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <a href="" class="flex items-center">
+                        <img src="../assets/assets/Logo0.svg" class="mr-3 h-8" alt="RMPlaysMC Studios Logo" />
+        </header>
+    </main>
 </body>
 </html>
